eda4976690d33afe9ef0317942dd61b17f0f9f3d
Hal Seki <hal@plants-web.jp>
2020-08-31 19:55:16 +0900

    use sidekick

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Specify active_job sidekiq adapter
+  config.active_job.queue_adapter = :sidekiq
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+#
+require "sidekiq/web"
 Rails.application.routes.draw do
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
 end
