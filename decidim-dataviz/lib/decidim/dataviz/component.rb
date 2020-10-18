dd75262ba250467cbc8d106f8dcc141b71553041
Hal Seki <hal@plants-web.jp>
2020-10-18 20:58:29 +0900

    removed unused module

--- decidim-dataviz/lib/decidim/dataviz/component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_dependency "decidim/components/namer"
-
-Decidim.register_component(:dataviz) do |component|
-  component.engine = Decidim::Dataviz::Engine
-  component.icon = "decidim/dataviz/icon.svg"
-  component.stylesheet = "decidim/dataviz/dataviz"
-end
