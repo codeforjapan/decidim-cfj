2fa804f85db946ac9e1ac2dd6755c5b7780426bd
Hal Seki <hal@plants-web.jp>
2020-10-21 08:26:13 +0900

    modify fog setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,14 @@
 if Rails.application.secrets.aws_access_key_id.present?
   require "carrierwave/storage/fog"
 
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
   CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_public = false
-    config.asset_host = 'https://s3.amazonaws.com/cfj-decidim'
+    config.fog_directory  = 'cfj-decidim'
+    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/cfj-decidim'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,
@@ -18,7 +21,6 @@ if Rails.application.secrets.aws_access_key_id.present?
       # host:                  's3.ap-northeast-1.amazonaws.com',
       # endpoint:              'https://s3.example.com:8080'
     }
-    config.fog_directory  = ENV.fetch("AWS_BUCKET_NAME", 'cfj-decidim')
     # config.fog_public     = false
     config.fog_attributes = {
       'Cache-Control' => "max-age=#{365.day.to_i}",
