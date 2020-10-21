b42fa229cd1a93010d81ce7b32f692fc33f0b02d
Hal Seki <hal@plants-web.jp>
2020-10-21 23:45:52 +0900

    change email from

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -5,7 +5,7 @@ Decidim.configure do |config|
   config.application_name = "Code for Japan Decidim"
 
   # The email that will be used as sender in all emails from Decidim
-  config.mailer_sender = "decidim@code4japan.org"
+  config.mailer_sender = "info@diycities.jp"
 
   # Sets the list of available locales for the whole application.
   #
