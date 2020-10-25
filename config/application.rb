6adfc41be43e20295e52830cfde23c04ac07f70d
takahashim <maki@rubycolor.org>
2020-10-25 19:37:30 +0900

    use rspec instead of minitest

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module DecidimApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      # remove some specs
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
