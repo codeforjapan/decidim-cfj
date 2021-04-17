19b32298d9e6db3acf6f9b58f9107b178d710403
ayuki_j <ajjt66554@gmail.com>
2021-04-17 23:54:13 +0900

    update migrate

---
+++ db/migrate/20210417144654_fix_counters_for_copied_proposals.decidim_proposals.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+# This migration comes from decidim_proposals (originally 20210318082934)
+
+class FixCountersForCopiedProposals < ActiveRecord::Migration[5.2]
+  def up
+    copies_ids = Decidim::ResourceLink
+                 .where(
+                   name: "copied_from_component",
+                   from_type: "Decidim::Proposals::Proposal",
+                   to_type: "Decidim::Proposals::Proposal"
+                 ).pluck(:to_id)
+
+    Decidim::Proposals::Proposal.where(id: copies_ids).find_each(&:update_comments_count)
+  end
+
+  def down; end
+end
