94394eb2af06c5ab820f5f4b4d06ea64bdee71ab
takahashim <takahashimm@gmail.com>
2021-05-10 19:25:20 +0900

    fix workflow form name

--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Decidim::Verifications.register_workflow(:user_extension) do |workflow|
-  workflow.form = "UserExtensionAuthorizationHandler"
+  workflow.form = "Decidim::UserExtensionAuthorizationHandler"
 end
