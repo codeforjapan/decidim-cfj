3dfa6e581534916be94a333ce4f6237515e5c406
takahashim <takahashimm@gmail.com>
2021-05-14 19:10:38 +0900

    use engine instead of form

--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
-Decidim::Verifications.register_workflow(:user_extension_authorization_handler) do |workflow|
-  workflow.form = "UserExtensionAuthorizationHandler"
+Decidim::Verifications.register_workflow(:user_extension) do |workflow|
+  workflow.engine = Decidim::UserExtension::Engine
+  workflow.admin_engine = Decidim::UserExtension::AdminEngine
 end
