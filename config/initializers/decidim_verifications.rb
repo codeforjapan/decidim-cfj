8d612b0405371561f9b7be7fd20cbf923269862c
takahashim <takahashimm@gmail.com>
2021-05-14 20:27:24 +0900

    remove old workflow

--- config/initializers/decidim_verifications.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Decidim::Verifications.register_workflow(:user_extension) do |workflow|
-  workflow.engine = Decidim::UserExtension::Engine
-  workflow.admin_engine = Decidim::UserExtension::AdminEngine
-end
