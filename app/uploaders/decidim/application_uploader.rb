c43c08dc482d9e8d828bd0e13e57bd93de8be5cf
takahashim <takahashimm@gmail.com>
2022-01-13 19:41:56 +0900

    Update `Decidim::ApplicationUploader`; Fix #322

--- app/uploaders/decidim/application_uploader.rb
+++ app/uploaders/decidim/application_uploader.rb
@@ -5,6 +5,8 @@ module Decidim
   # hold the uploads configuration, so you should inherit from this class and
   # then tweak any configuration you need.
   class ApplicationUploader < CarrierWave::Uploader::Base
+    include CarrierWave::MiniMagick
+
     process :validate_inside_organization
 
     # Override the directory where uploaded files will be stored.
@@ -15,6 +17,28 @@ module Decidim
       return File.join(Decidim.base_uploads_path, default_path) if Decidim.base_uploads_path.present?
 
       default_path
+    end
+
+    # When the uploaded content can't be processed, we want to make sure
+    # not to expose internal tools errors to the users.
+    # We'll show a generic error instead.
+    def manipulate!
+      super
+    rescue CarrierWave::ProcessingError => e
+      Rails.logger.error(e)
+      raise CarrierWave::ProcessingError, I18n.t("carrierwave.errors.general")
+    end
+
+    # As of Carrierwave 2.0 fog_provider method has been deprecated, and is throwing RuntimeError
+    # RuntimeError: Carrierwave fog_provider not supported: DEPRECATION WARNING: #fog_provider is deprecated...
+    # We are attempting to fetch the provider from credentials, if not we consider to be file
+    def provider
+      fog_credentials.fetch(:provider, "file").downcase
+    end
+
+    # We overwrite the downloader to be able to fetch some elements from URL.
+    def downloader
+      Decidim::Downloader
     end
 
     # Overwrite: If the content block is in preview mode, then we show the
