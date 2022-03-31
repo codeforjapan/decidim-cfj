0ba0ce5373a27ffb91e2862b9ae1a2dd3c4972ef
takahashim <takahashimm@gmail.com>
2022-04-01 01:52:08 +0900

    Fix: do not display changeset if it is too large

--- app/controllers/decidim/debates/versions_controller.rb
+++ app/controllers/decidim/debates/versions_controller.rb
@@ -8,8 +8,18 @@ module Decidim
       include Decidim::ApplicationHelper
       include Decidim::ResourceVersionsConcern
 
+      OBJECT_CHANGE_SIZE_LIMIT = 1_000_000
+
       def versioned_resource
         @versioned_resource ||= present(Debate.where(component: current_component).find(params[:debate_id]))
+      end
+
+      def show
+        description = current_version.object_changes
+        if description.size > OBJECT_CHANGE_SIZE_LIMIT
+          flash[:alert] = I18n.t("debates.versions.too_large_changeset", scope: "decidim.debates")
+          redirect_to action: :index
+        end
       end
     end
   end
