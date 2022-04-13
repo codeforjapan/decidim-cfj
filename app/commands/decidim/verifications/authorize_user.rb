e0e110fa7a55fa8ae267aa74bb829e5b6362ba55
takahashim <takahashimm@gmail.com>
2022-04-13 23:15:38 +0900

    Copy files from v0.24.3

---
+++ app/commands/decidim/verifications/authorize_user.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Verifications
+    # A command to authorize a user with an authorization handler.
+    class AuthorizeUser < Rectify::Command
+      # Public: Initializes the command.
+      #
+      # handler - An AuthorizationHandler object.
+      def initialize(handler)
+        @handler = handler
+      end
+
+      # Executes the command. Broadcasts these events:
+      #
+      # - :ok when everything is valid.
+      # - :invalid if the handler wasn't valid and we couldn't proceed.
+      #
+      # Returns nothing.
+      def call
+        if handler.invalid?
+          conflict = create_verification_conflict
+          notify_admins(conflict) if conflict.present?
+
+          return broadcast(:invalid)
+        end
+
+        Authorization.create_or_update_from(handler)
+
+        broadcast(:ok)
+      end
+
+      private
+
+      attr_reader :handler
+
+      def notify_admins(conflict)
+        Decidim::EventsManager.publish(
+          event: "decidim.events.verifications.managed_user_error_event",
+          event_class: Decidim::Verifications::ManagedUserErrorEvent,
+          resource: conflict,
+          affected_users: Decidim::User.where(admin: true)
+        )
+      end
+
+      def create_verification_conflict
+        authorization = Decidim::Authorization.find_by(unique_id: handler.unique_id)
+        return if authorization.blank?
+
+        conflict = Decidim::Verifications::Conflict.find_or_initialize_by(
+          current_user: handler.user,
+          managed_user: authorization.user,
+          unique_id: handler.unique_id
+        )
+
+        conflict.update(times: conflict.times + 1)
+
+        conflict
+      end
+    end
+  end
+end
