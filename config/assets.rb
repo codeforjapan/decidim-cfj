0b6030aa0cb38792b44b176ababb79eb390b9818
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2022-05-16 15:47:26 +0900

    fix lint

--- config/assets.rb
+++ config/assets.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 # This file is located at `config/assets.rb` of your module.
 
 # Define the base path of your module. Please note that `Rails.root` may not be
 # used because we are not inside the Rails environment when this file is loaded.
-base_path = File.expand_path("..", __dir__)
+# base_path = File.expand_path("..", __dir__)
 
 # If you want to import some extra SCSS files in the Decidim main SCSS file
 # without adding any extra stylesheet inclusion tags, you can use the following
