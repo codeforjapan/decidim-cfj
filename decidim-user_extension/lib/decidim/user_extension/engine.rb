3e7eb0f00449f00d21ceb7103c6c120fc15cdbf2
takahashim <takahashimm@gmail.com>
2022-08-19 23:45:45 +0900

    UserExtension: Fix initializers to load modules properly
    * AdminEngine: do not include NeedsUserExtension, because DecidimController (superclass of Decidim::Admin::ApplicationController) includes it
    * Engine: use `config.to_prepare do ... end` to fix load timing

--- decidim-user_extension/lib/decidim/user_extension/engine.rb
+++ decidim-user_extension/lib/decidim/user_extension/engine.rb
@@ -26,44 +26,46 @@ module Decidim
       end
 
       initializer "decidim_user_extension.engine_additions" do
-        Decidim::RegistrationForm.class_eval do
-          include UserExtension::FormsDefinitions
-        end
+        config.to_prepare do
+          Decidim::RegistrationForm.class_eval do
+            include UserExtension::FormsDefinitions
+          end
 
-        Decidim::OmniauthRegistrationForm.class_eval do
-          include UserExtension::FormsDefinitions
-        end
+          Decidim::OmniauthRegistrationForm.class_eval do
+            include UserExtension::FormsDefinitions
+          end
 
-        Decidim::AccountForm.class_eval do
-          include UserExtension::FormsDefinitions
-        end
+          Decidim::AccountForm.class_eval do
+            include UserExtension::FormsDefinitions
+          end
 
-        Decidim::CreateRegistration.class_eval do
-          prepend UserExtension::CreateCommandsOverrides
-        end
+          Decidim::CreateRegistration.class_eval do
+            prepend UserExtension::CreateCommandsOverrides
+          end
 
-        Decidim::UpdateAccount.class_eval do
-          prepend UserExtension::UpdateCommandsOverrides
-        end
+          Decidim::UpdateAccount.class_eval do
+            prepend UserExtension::UpdateCommandsOverrides
+          end
 
-        Decidim::DestroyAccount.class_eval do
-          prepend UserExtension::DestroyCommandsOverrides
-        end
+          Decidim::DestroyAccount.class_eval do
+            prepend UserExtension::DestroyCommandsOverrides
+          end
 
-        Decidim::CreateOmniauthRegistration.class_eval do
-          prepend UserExtension::CreateOmniauthCommandsOverrides
-        end
+          Decidim::CreateOmniauthRegistration.class_eval do
+            prepend UserExtension::CreateOmniauthCommandsOverrides
+          end
 
-        DecidimController.class_eval do
-          include Decidim::UserExtension::Concerns::Controllers::NeedsUserExtension
-        end
+          DecidimController.class_eval do
+            include Decidim::UserExtension::Concerns::Controllers::NeedsUserExtension
+          end
 
-        Decidim::Devise::RegistrationsController.class_eval do
-          prepend Decidim::UserExtension::Concerns::Controllers::AddUserExtensionForm
-        end
+          Decidim::Devise::RegistrationsController.class_eval do
+            prepend Decidim::UserExtension::Concerns::Controllers::AddUserExtensionForm
+          end
 
-        Decidim::Devise::OmniauthRegistrationsController.class_eval do
-          prepend Decidim::UserExtension::Concerns::Controllers::OmniauthAddUserExtensionForm
+          Decidim::Devise::OmniauthRegistrationsController.class_eval do
+            prepend Decidim::UserExtension::Concerns::Controllers::OmniauthAddUserExtensionForm
+          end
         end
       end
     end
