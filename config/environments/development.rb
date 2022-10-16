d177d239ad44f55a592fefd68c8fe1311e9738c9
takahashim <takahashimm@gmail.com>
2022-10-16 21:34:47 +0900

    Fix CSRF check; allow different ports

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  # Disable origin-checking CSRF mitigation to allow different ports.
+  config.action_controller.forgery_protection_origin_check = false
+
   # No precompilation on demand on first request
 end
