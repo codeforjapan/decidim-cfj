da0c963c074dcd3cfb7c404ac9368a6d060bba41
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2022-10-27 14:55:49 +0900

    refactor: change image upload size limit

---
+++ app/uploaders/decidim/image_uploader.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Decidim
+  # This class deals with uploading hero images to ParticipatoryProcesses.
+  class ImageUploader < ApplicationUploader
+    def validable_dimensions
+      true
+    end
+
+    def content_type_allowlist
+      extension_allowlist.map { |ext| "image/#{ext}" }
+    end
+
+    # Fetches info about different variants, their processors and dimensions
+    def dimensions_info
+      return if variants.blank?
+
+      variants.transform_values do |variant|
+        {
+          processor: variant.keys.first,
+          dimensions: variant.values.first
+        }
+      end
+    end
+
+    # Add a white list of extensions which are allowed to be uploaded.
+    # For images you might use something like this:
+    def extension_allowlist
+      Decidim.organization_settings(model).upload_allowed_file_extensions_image
+    end
+
+    def max_image_height_or_width
+      8000
+    end
+
+    private
+
+    def maximum_upload_size
+      Decidim.organization_settings(model).upload_maximum_file_size
+    end
+  end
+end
+
