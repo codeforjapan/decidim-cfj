9d4d43ef5a2b787f42c0eab7b498f95489220b48
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2022-11-17 19:50:42 +0900

    feat: update packages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_135828) do
+ActiveRecord::Schema.define(version: 2022_11_17_104040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
