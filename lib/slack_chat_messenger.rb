57a0013f82dd79ec184e3fba1763db3c31ad1aad
takahashim <takahashimm@gmail.com>
2022-11-17 20:26:14 +0900

    refactor: Move SlackChatMessenger to use in Decidim apps

---
+++ lib/slack_chat_messenger.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class SlackChatMessenger
+  def self.notify(channel:, message:)
+    unless channel && message
+      Rails.logger.error "Cannot send messages to slack!"
+      return
+    end
+    client = Slack::Web::Client.new
+    client.chat_postMessage(channel: channel, text: message, as_user: true)
+  end
+end
