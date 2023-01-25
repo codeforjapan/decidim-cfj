cec48d297ddbb3a56e4301287512c103ec37d070
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2023-01-25 17:29:42 +0900

    feat: add comment orderable params

--- app/controllers/concerns/decidim/proposals/cfj_orderable.rb
+++ app/controllers/concerns/decidim/proposals/cfj_orderable.rb
@@ -2,15 +2,21 @@
 
 module Decidim
   module Proposals
-    module CookieOrderable
+    module CfjOrderable
       private
 
       def default_order
-        order_by_cookie || super
+        order_by_query_param || order_by_cookie || super
+      end
+
+      def order_by_query_param
+        param = params[:orderable]
+        param if param && available_orders.include?(param)
       end
 
       def order_by_cookie
         cookie = cookies[order_cookie_name]
+        param = params[:orderable]
         cookie if cookie && available_orders.include?(cookie)
       end
 
@@ -21,7 +27,6 @@ module Decidim
       def detect_order(candidate)
         detected = available_orders.detect { |order| order == candidate }
         cookies[order_cookie_name] = detected if detected
-
         detected
       end
     end
