06e1d5c967697097262ff37eb82f583863bc2c79
takahashim <takahashimm@gmail.com>
2023-02-20 02:00:10 +0900

    remove Decidim::Verifications::Conflict

--- app/commands/decidim/organizations/destroy_organization.rb
+++ app/commands/decidim/organizations/destroy_organization.rb
@@ -24,6 +24,13 @@ module Decidim
           end
         end
 
+        Decidim::Verifications::Conflict.find_each do |conflict|
+          if conflict.current_user.organization == organization
+            puts "destroy verifications_conflict id: #{conflict.id}"
+            conflict.destroy!
+          end
+        end
+
         Decidim::DecidimAwesome::EditorImage.where(organization: organization).delete_all
 
         Decidim::ActionLog.where(organization: organization).delete_all
