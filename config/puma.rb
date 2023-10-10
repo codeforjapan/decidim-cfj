fa5128726e4972bbb671ab4227587eeff5806b0f
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2023-10-10 21:23:23 +0900

    refactor: fix lint

--- config/puma.rb
+++ config/puma.rb
@@ -43,7 +43,7 @@ end
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { 2 } unless Rails.env.development?
+workers ENV.fetch("WEB_CONCURRENCY", 2) unless Rails.env.development?
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
