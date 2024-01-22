b9fc5fac0d1ca1b01329e048b9cc5d54d4d7723c
takahashim <takahashimm@gmail.com>
2024-01-22 23:05:49 +0900

    WIP

--- config/initializers/decidim_override.rb
+++ config/initializers/decidim_override.rb
@@ -32,10 +32,68 @@ Rails.application.config.to_prepare do
   end
 
   module Decidim
+    module Comments
+      class SortedComments < Decidim::Query
+        # override
+        def query
+          scope = base_scope
+                  .includes(:author, :user_group, :up_votes, :down_votes)
+
+          case @options[:order_by]
+          when "older"
+            order_by_older(scope)
+          when "recent"
+            order_by_recent(scope)
+          when "best_rated"
+            order_by_best_rated(scope)
+          when "most_discussed"
+            order_by_most_discussed(scope)
+          else # rubocop:disable Lint/DuplicateBranch
+            order_by_older(scope)
+          end
+        end
+      end
+
+      class CommentsController < Decidim::Comments::ApplicationController
+        private
+
+        # override
+        def order
+          params_order = params.fetch(:order, nil)
+          if params_order
+            cookies["comment_default_order"] = params_order if cookies[Decidim.config.consent_cookie_name].present? # cookies_accepted?
+            params_order
+          elsif cookies["comment_default_order"] && cookies[Decidim.config.consent_cookie_name].present? # cookies_accepted?
+            cookies["comment_default_order"]
+          else
+            "older"
+          end
+        end
+      end
+
+      module CommentsHelper
+        # Override
+        def inline_comments_for(resource, options = {})
+          return unless resource.commentable?
+
+          cell(
+            "decidim/comments/comments",
+            resource,
+            machine_translations: machine_translations_toggled?,
+            single_comment: params.fetch("commentId", nil),
+            order: options[:order] || params["orderable"] || cookies["comment_default_order"],
+            polymorphic: options[:polymorphic]
+          ).to_s
+        end
+      end
+    end
+  end
+
+  module Decidim
     module Map
       class DynamicMap < Map::Frontend
         class Builder < Decidim::Map::Frontend::Builder
-          # Overwrite
+          # Override
           def map_element(html_options = {})
             opts = view_options
             opts["markers"] = opts["markers"].reject { |item| item[:latitude].nil? || item[:latitude].nan? } if opts["markers"].present?
