652974b8d3902cdbfab7304a120c7a79ba3fa818
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2024-03-18 17:58:55 +0900

    wip: update check.yaml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = Logger::Formatter.new
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets.smtp_address,
     port: Rails.application.secrets.smtp_port,
