db81eeaa41813d5f562ea71f5e205f5cde4b7b73
takahashim <takahashimm@gmail.com>
2024-07-24 20:30:06 +0900

    use redis for cache and session store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_CACHE_URL", nil) }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
