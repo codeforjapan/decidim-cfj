970178ebea1265faab77c5034fa793d7c17aff34
takahashim <takahashimm@gmail.com>
2024-12-26 15:15:25 +0900

    use TagEdit instead of HtmlEdit
    TagEdit is new HTML Tag Editor as a TipTap extension.

--- app/packs/src/decidim/editor/extensions/decidim_kit/index.js
+++ app/packs/src/decidim/editor/extensions/decidim_kit/index.js
@@ -16,7 +16,7 @@ import Link from "src/decidim/editor/extensions/link";
 import Mention from "src/decidim/editor/extensions/mention";
 import VideoEmbed from "src/decidim/editor/extensions/video_embed";
 import Emoji from "src/decidim/editor/extensions/emoji";
-import HtmlEdit from "src/decidim/cfj/editor/extensions/html_edit";
+import TagEdit from "src/decidim/cfj/editor/extensions/tag_edit";
 
 export default Extension.create({
   name: "decidimKit",
@@ -53,7 +53,7 @@ export default Extension.create({
       Indent,
       OrderedList,
       CodeBlock,
-      HtmlEdit,
+      TagEdit,
       Underline
     ];
 
