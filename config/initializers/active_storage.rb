d341e6dd25b09e84bd890e101dcde9b5ad076c74
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2025-03-12 15:57:36 +0900

    feat: update decidim v0.29.2

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require Rails.root.join("app/previewers/heic_previewer")
+
 Rails.application.configure do
   config.active_storage.previewers << HeicPreviewer
   config.active_storage.variable_content_types << "image/heic"
