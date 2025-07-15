261eee5f5536bc7e9f2c75194b43251576950b59
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2025-07-15 17:54:51 +0900

    fix: remove redundant `to_s` in decidim_override initializer

--- config/initializers/decidim_override.rb
+++ config/initializers/decidim_override.rb
@@ -73,7 +73,7 @@ Rails.application.config.to_prepare do
             single_comment: params.fetch("commentId", nil),
             order: options[:order] || params["orderable"] || cookies["comment_default_order"],
             polymorphic: options[:polymorphic]
-          ).to_s
+          )
         end
       end
     end
