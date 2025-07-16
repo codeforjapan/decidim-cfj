730e19ce1225f7ba6549ea3f4844c7526d1da8fd
takahashim <takahashimm@gmail.com>
2025-07-16 22:53:34 +0900

    feat: add settings for components

---
+++ app/jobs/purge_component_cache_job.rb
@@ -0,0 +1,19 @@
+class PurgeComponentCacheJob < ApplicationJob
+  def perform(component_id)
+    component = Decidim::Component.find(component_id)
+    clear_all_related_caches(component)
+  end
+
+  private
+
+  def clear_all_related_caches(component)
+    patterns = [
+      "cells/decidim/comments/comment_form/*",
+    ]
+
+    patterns.each do |pattern|
+      count = Rails.cache.delete_matched(pattern)
+      Rails.logger.info "Purged #{count} cache entries matching #{pattern}"
+    end
+  end
+end
