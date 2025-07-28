69642e3c437b24bba6e6ee5496d67106dfcbfe0f
takahashim <takahashimm@gmail.com>
2025-07-29 00:34:16 +0900

    chore: uploader settings simplified

--- app/uploaders/decidim/organization_mobile_logo_uploader.rb
+++ app/uploaders/decidim/organization_mobile_logo_uploader.rb
@@ -3,17 +3,7 @@
 module Decidim
   class OrganizationMobileLogoUploader < ImageUploader
     set_variants do
-      {
-        small: { resize_to_fit: [180, 60] },
-        medium: { resize_to_fit: [360, 120] }
-      }
-    end
-
-    def dimensions_info
-      {
-        small: { processor: :resize_to_fit, dimensions: [180, 60] },
-        medium: { processor: :resize_to_fit, dimensions: [360, 120] }
-      }
+      { medium: { resize_to_fit: [360, 120] } }
     end
   end
 end
