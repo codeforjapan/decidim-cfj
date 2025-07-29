339fa28b5bed9de3872ecc76bf8863509ec4f4e7
takahashim <takahashimm@gmail.com>
2025-07-29 10:15:50 +0900

    fix logo size

--- app/uploaders/decidim/organization_mobile_logo_uploader.rb
+++ app/uploaders/decidim/organization_mobile_logo_uploader.rb
@@ -3,7 +3,7 @@
 module Decidim
   class OrganizationMobileLogoUploader < RecordImageUploader
     set_variants do
-      { medium: { resize_to_fit: [360, 120] } }
+      { medium: { resize_to_fit: [360, 100] } }
     end
   end
 end
