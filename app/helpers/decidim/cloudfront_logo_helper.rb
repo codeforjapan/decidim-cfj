f3a6f993d8bc406ecba30b0248a212159b2948bb
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2025-09-04 19:25:48 +0900

    fix: lint

--- app/helpers/decidim/cloudfront_logo_helper.rb
+++ app/helpers/decidim/cloudfront_logo_helper.rb
@@ -4,7 +4,7 @@ module Decidim
   module CloudfrontLogoHelper
     # CloudFront経由で直接S3のロゴURLを生成する
     # バリアントが未処理でも直接S3のURLを返す
-    def cloudfront_logo_url(organization, logo_type = :logo, variant = :medium)
+    def cloudfront_logo_url(organization, logo_type = :logo)
       return unless organization.send(logo_type).attached?
 
       blob = organization.send(logo_type).blob
