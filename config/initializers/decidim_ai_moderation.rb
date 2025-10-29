2bc825fe7de5dc6632feba4cf28dd0df3ec86018
takahashim <takahashimm@gmail.com>
2025-10-29 16:34:21 +0900

    rubocop

--- config/initializers/decidim_ai_moderation.rb
+++ config/initializers/decidim_ai_moderation.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Decidim::Ai::CommentModeration.configure do |config|
   config.openai_api_key = ENV.fetch("OPENAI_API_KEY", nil)
   config.enabled_hosts = ["localhost", "dummy.example.com"]
