67775ffed4c0812d48deb585e18ffda21f6bf6c6
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2026-05-18 21:11:30 +0900

    chore: upgrade decidim from 0.30.5 to 0.30.9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_02_10_053626) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_18_113546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
   enable_extension "pg_bigm"
@@ -421,7 +421,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_02_10_053626) do
     t.jsonb "title"
     t.integer "weight", default: 0, null: false
     t.jsonb "description"
-    t.integer "total_budget", default: 0
+    t.bigint "total_budget", default: 0
     t.integer "decidim_component_id"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
