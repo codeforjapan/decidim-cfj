cfbc7dad58a8884c179d92022dd3288288e9f53a
takahashim <takahashimm@gmail.com>
2026-05-23 00:14:47 +0900

    chore: copy from v0.30.9

---
+++ app/services/decidim/iframe_disabler.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Decidim
+  class IframeDisabler
+    def initialize(text, _options = {})
+      @text = text
+    end
+
+    def perform
+      @document = Nokogiri::HTML::DocumentFragment.parse(@text)
+      disable_iframes(@document)
+      document.to_html
+    end
+
+    private
+
+    attr_reader :document
+
+    def disable_iframes(node)
+      if node.name == "iframe"
+        # Default title for accessibility
+        node["title"] = I18n.t("decidim.shared.embed.title") if node["title"].blank?
+        # Disable scrollbar for some embed services
+        node["scrolling"] = "no"
+        orig_node = node.to_s
+        node.replace(%(<div class="disabled-iframe"><!-- #{orig_node} --></div>))
+      end
+
+      node.children.each do |child|
+        disable_iframes(child)
+      end
+    end
+  end
+end
